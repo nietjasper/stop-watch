ef21d039b41ce7dea3e63324e4522051b5c6c1f2
Jasper Wolthuis <jasperwolthuis@gmail.com>
2015-04-16 00:00:44 +0200

    Added timer tick, and added functions to bottons

--- componets/app-main.js
+++ componets/app-main.js
@@ -1,4 +1,64 @@
 polymer({
   
+  elapsedTime: 0,
+  
+  lastTime: 0,
+  
+  stopped: true,
+  
+  ready: function(){
+    
+    window.requestAnimationFrame(this.updateTime.bind(this));
+    
+  },
+  
+  start: function(){
+    
+    this.stopped = false;
+    
+  },
+  
+  stop: function(){
+    
+    this.stopped = true;
+    
+  },
+  
+  restart: function(){
+    
+    this.elapsedTime = 0;
+    
+  },
+  
+  updateTime: function(e){
+    
+    
+    if(this.stopped){
+      
+      this.lastTime = 0;
+      
+    }
+    
+    else{
+      
+      if(this.lastTime == 0){
+        
+        this.lastTime = e;
+        
+      }
+      
+      else{
+        
+        this.elapsedTime += e - this.lastTime;
+        
+        this.lastTime = e;
+        
+      }
+      
+    }
+    
+    window.requestAnimationFrame(this.updateTime.bind(this));
+  
+  }
   
 })
